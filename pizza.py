e69a961512d18977d0a147452a97c3e82497fb87
19zjones <42579155+19zjones@users.noreply.github.com>
2018-08-31 10:21:59 -0400

    Add files via upload

--- pizza.py
+++ pizza.py
@@ -1,14 +1,14 @@
 """Enter possible pizza ingredients below but inside the triple quote:
-
-
-
-
-
-
-
-
-
-
-
-
+Cheese
+Pepperoni
+Mushrooms
+Dough
+Sauce
+Burger
+Bacon
+Sausage
+Banna Peppers
+Onions
+Ranch
+Ham
 """
